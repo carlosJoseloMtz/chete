bb8ec059ff6825131fd7b2d190c9f444a96a48df
rafael <rafael2023@gmail.com>
2018-02-19 21:23:58 -0600

    integrate mongoose and add test file to use mongo db with rest api

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,15 @@ import express from 'express'
 import load from 'express-load'
 
 const app = express()
+const mongoose = require("mongoose")
+const config = require("./database/config.js")
 
-// load the full application!!
+mongoose.connect(config.setConfig.database)
+const dataController = require('./controllers/test')
+const dataController2 = require('./controllers/addtest')
+app.get('/findAll', dataController.getData)
+app.post('/create', dataController2.postData)
+
 load('app/config',
   { verbose: true })
   .then('app/servicelsoader')
