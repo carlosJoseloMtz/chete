cc7c6019711c486b9d34f81067a0b6865c135aa4
rafael <rafael2023@gmail.com>
2018-02-26 18:17:01 -0600

    add wherehouse model addresmodel

---
+++ backend/core/models/addressmodel.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose'
+
+const AddressModel = {
+
+    street: {
+      type: String,
+      required: true
+    },
+
+    city: {
+      type: String,
+      required: true
+    },
+
+    state: {
+      type: String,
+      required: true
+    },
+
+    zipcode: {
+      type: Number,
+      required: true
+    },
+
+    country: {
+      type: String,
+      required: true
+    }
+
+}
+
+export default { AddressModel }
